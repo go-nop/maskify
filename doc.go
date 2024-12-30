426edfa53dc98ad440325d3998fe37d2107eb536
n0l3r <naufal.120140044@student.itera.ac.id>
2024-12-30 15:50:32 +0700

    chore: go mod init

---
+++ doc.go
@@ -0,0 +1,5 @@
+// A Go library to sanitize and mutate struct fields before validation.
+// It helps clean and normalize data, ensuring it's in a valid and consistent state, reducing validation errors.
+// Customize sanitization rules for your structs to handle user input or external data more safely and efficiently.
+// See the README for more information and examples.
+package sanitizer
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/go-nop/sanitizer
+
+go 1.23.2
